d667141b629e724e999118ae35c3d48619e1350c
guilhere_lf <gui_folieti@hotmail.com>
2025-06-02 22:47:25 -0300

    att pag cadastro

--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -1,11 +1,79 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CadastroService } from './cadastro.service';
 
 @Component({
   selector: 'app-cadastro',
-  imports: [],
   templateUrl: './cadastro.component.html',
-  styleUrl: './cadastro.component.css'
+  styleUrls: ['./cadastro.component.css']
 })
-export class CadastroComponent {
+export class CadastroComponent implements OnInit {
+  cadastroForm!: FormGroup;
 
+  constructor(
+    private fb: FormBuilder,
+    private cadastroService: CadastroService
+  ) {}
+
+  ngOnInit(): void {
+    this.cadastroForm = this.fb.group({
+      nome: ['', Validators.required],
+      cpf: ['', Validators.required],
+      telefone: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      rua: [''],
+      numero: [''],
+      cidade: [''],
+      estado: [''],
+      cep: [''],
+      senha: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.cadastroForm.valid) {
+      const usuario = this.cadastroForm.value;
+      console.log('Cadastro enviado:', usuario);
+      alert('Cadastro realizado com sucesso!');
+      this.cadastroForm.reset();
+    } else {
+      alert('Preencha todos os campos obrigatórios corretamente.');
+    }
+  }
+
+  cadastrar(): void {
+  if (this.cadastroForm.valid) {
+    const usuario = this.cadastroForm.value;
+
+    // Exibe os dados no console
+    console.log('Dados do usuário:');
+    console.log('Nome:', usuario.nome);
+    console.log('CPF:', usuario.cpf);
+    console.log('Telefone:', usuario.telefone);
+    console.log('Email:', usuario.email);
+    console.log('Endereço:', {
+      rua: usuario.rua,
+      numero: usuario.numero,
+      cidade: usuario.cidade,
+      estado: usuario.estado,
+      cep: usuario.cep
+    });
+    console.log('Senha:', usuario.senha);
+
+    // Aqui você pode chamar o serviço para salvar
+    this.cadastroService.cadastrarUsuario(usuario).subscribe(
+      res => {
+        console.log('Usuário cadastrado com sucesso:', res);
+        alert('Cadastro realizado com sucesso!');
+        this.cadastroForm.reset();
+      },
+      err => {
+        console.error('Erro no cadastro:', err);
+        alert('Erro ao cadastrar. Tente novamente.');
+      }
+    );
+  } else {
+    alert('Preencha todos os campos obrigatórios corretamente.');
+  }
+  }
 }
---
+++ src/app/cadastro/cadastro.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CadastroService {
+  private apiUrl = 'http://localhost:3000/api';
+
+  constructor(private http: HttpClient) {}
+
+  cadastrarUsuario(usuario: {
+    nome: string,
+    email: string,
+    senha: string,
+    telefone: string,
+    cpf: string,
+    endereco: {
+      rua: string,
+      numero: string,
+      cidade: string,
+      estado: string,
+      cep: string
+    }
+  }): Observable<any> {
+    return this.http.post(`${this.apiUrl}/cadastro`, usuario);
+  }
+}
