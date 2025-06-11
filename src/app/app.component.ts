729bba85218d424c61f035b6c0ac192ccfb965af
guilhere_lf <gui_folieti@hotmail.com>
2025-06-11 09:32:52 -0300

    Add footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { Router, RouterOutlet } from '@angular/router';
 import { MenuComponent } from './menu/menu.component';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [MenuComponent, RouterOutlet, CommonModule, HttpClientModule],
+  imports: [MenuComponent, RouterOutlet, CommonModule, HttpClientModule, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
