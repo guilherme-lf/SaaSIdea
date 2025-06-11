d515bb48c44b70d55ecb2044ea9512b8ab48237a
guilhere_lf <gui_folieti@hotmail.com>
2025-06-11 09:52:21 -0300

    Atualizações mais responsivas

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-landing',
@@ -9,5 +9,35 @@ import { Component } from '@angular/core';
   styleUrl: './landing.component.css'
 })
 export class LandingComponent {
+
+  @ViewChild('scrollContainer', { static: false }) scrollContainer!: ElementRef;
+
+  planos = [
+    {
+      titulo: 'Plano Essencial',
+      descricao: 'Ideal para pequenos mercados que estão começando. Ferramentas básicas de controle.'
+    },
+    {
+      titulo: 'Plano Profissional',
+      descricao: 'Soluções avançadas para gestão de estoque e vendas em mercados médios.'
+    },
+    {
+      titulo: 'Plano Premium',
+      descricao: 'Automação completa para redes de supermercado com múltiplas filiais.'
+    },
+    {
+      titulo: 'Plano Empresarial',
+      descricao: 'Plano sob demanda com recursos personalizados e suporte prioritário.'
+    }
+  ];
+  
+  scrollLeft() {
+    this.scrollContainer.nativeElement.scrollBy({ left: -300, behavior: 'smooth' });
+  }
+
+  scrollRight() {
+    this.scrollContainer.nativeElement.scrollBy({ left: 300, behavior: 'smooth' });
+  }
+
 
 }
