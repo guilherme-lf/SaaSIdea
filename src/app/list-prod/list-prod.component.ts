520161ec5c78fe8659a8db9c6771a7670bed7fcb
guilhere_lf <gui_folieti@hotmail.com>
2025-06-18 14:57:04 -0300

    att list html e ts

--- src/app/list-prod/list-prod.component.ts
+++ src/app/list-prod/list-prod.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-list-prod',
-  imports: [],
+  standalone: true, 
+  imports: [HttpClient],
   templateUrl: './list-prod.component.html',
   styleUrl: './list-prod.component.css'
 })
-export class ListProdComponent {
+export class ListProdComponent implements OnInit {
+  produtos: any[] = [];
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.carregarProdutos();
+  }
+
+  carregarProdutos() {
+    this.http.get<any[]>('http://localhost:3000/api/produtos')
+      .subscribe({
+        next: (res) => this.produtos = res,
+        error: (err) => console.error('Erro ao carregar produtos:', err)
+      });
+  }
+
+
 
 }
