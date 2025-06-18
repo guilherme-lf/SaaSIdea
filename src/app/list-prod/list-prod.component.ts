935ccf70c7b3f6c497c55646b381e21792f25e57
guilhere_lf <gui_folieti@hotmail.com>
2025-06-18 14:50:57 -0300

    add pag list-prod

---
+++ src/app/list-prod/list-prod.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-prod',
+  imports: [],
+  templateUrl: './list-prod.component.html',
+  styleUrl: './list-prod.component.css'
+})
+export class ListProdComponent {
+
+}
