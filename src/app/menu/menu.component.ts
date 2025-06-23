4e264c056104797ea2e5bd968620d94bb002e2ca
guilhere_lf <gui_folieti@hotmail.com>
2025-06-23 15:02:44 -0300

    Correção do ssr

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,11 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
-  imports: [],
+  imports: [ CommonModule ],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
 export class MenuComponent {
+
+  isCollapsed = false;
+
+  toggleSidebar() {
+    this.isCollapsed = !this.isCollapsed;
+  }
 
 }
