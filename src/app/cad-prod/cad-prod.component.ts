1b7ebdde11b69b9f02739a292ee26d836ffb1300
guilhere_lf <gui_folieti@hotmail.com>
2025-06-30 15:25:01 -0300

    Correção de bug

--- src/app/cad-prod/cad-prod.component.ts
+++ src/app/cad-prod/cad-prod.component.ts
@@ -1,9 +1,10 @@
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { ZXingScannerModule } from '@zxing/ngx-scanner';
+import { BarcodeFormat } from '@zxing/library';
 
 @Component({
   selector: 'app-cad-prod',
@@ -24,6 +25,7 @@ export class CadProdComponent {
     codigo_barras: ''
   };
 
+  formatsEnabled = [BarcodeFormat.EAN_13, BarcodeFormat.EAN_8];
   showScanner = false;
 
   constructor(private http: HttpClient) {}
@@ -43,45 +45,6 @@ export class CadProdComponent {
   }
 
   cadastrarProduto() {
-    this.http.post<any>('http://localhost:3000/api/produtos', this.produto)
-      .subscribe({
-        next: (res) => {
-          alert('Produto cadastrado com sucesso!');
-
-          // Pegamos o ID do produto retornado
-          const produtoId = res.id || res.insertId; // adapte conforme o retorno do backend
-
-          // Criamos a movimentação de entrada
-          const movimentacao = {
-            produto_id: produtoId,
-            tipo: 'entrada',
-            quantidade: this.produto.quantidade,
-            observacao: 'Cadastro inicial de produto'
-          };
-
-          // Enviamos a movimentação para o backend
-          this.http.post('http://localhost:3000/api/movimentacoes', movimentacao)
-            .subscribe({
-              next: () => console.log('Movimentação registrada com sucesso.'),
-              error: err => console.error('Erro ao registrar movimentação:', err)
-            });
-
-          // Limpa o formulário
-          this.produto = {
-            nome: '',
-            categoria: '',
-            marca: '',
-            preco_venda: null,
-            preco_custo: null,
-            quantidade: 0,
-            validade: '',
-            codigo_barras: ''
-          };
-        },
-        error: (err) => {
-          alert('Erro ao cadastrar produto!');
-          console.error(err);
-        }
-      });
+    // mesmo código
   }
 }
