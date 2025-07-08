c52e6dc43689a97805c275a1b385c1cb0c873af6
guilhere_lf <gui_folieti@hotmail.com>
2025-07-08 16:23:42 -0300

    add components no menu

--- src/app/atendentes/atendentes.component.ts
+++ src/app/atendentes/atendentes.component.ts
@@ -1,11 +1,49 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-atendentes',
-  imports: [],
+  standalone: true,
+  imports: [ReactiveFormsModule, CommonModule, HttpClientModule],
   templateUrl: './atendentes.component.html',
   styleUrl: './atendentes.component.css'
 })
-export class AtendentesComponent {
 
+export class AtendentesComponent implements OnInit {
+  atendente = { nome: '', senha: '' };
+  atendentes: any[] = [];
+
+  constructor(private http: HttpClient) { }
+
+  
+  ngOnInit() {
+    this.listarAtendentes();
+  }
+
+  cadastrar() {
+    this.http.post('http://localhost:3000/api/atendentes', this.atendente).subscribe(() => {
+      this.atendente = { nome: '', senha: '' };
+      this.listarAtendentes();
+    });
+  }
+
+  listarAtendentes() {
+    this.http.get<any[]>('http://localhost:3000/api/atendentes').subscribe(res => {
+      this.atendentes = res;
+    });
+  }
+
+  excluir(id: number) {
+    if (confirm('Tem certeza que deseja excluir?')) {
+      this.http.delete(`http://localhost:3000/api/atendentes/${id}`).subscribe(() => {
+        this.listarAtendentes();
+      });
+    }
+  }
+
+  editar(at: any) {
+    this.atendente = { ...at }; // carrega dados no formulário
+  }
 }
