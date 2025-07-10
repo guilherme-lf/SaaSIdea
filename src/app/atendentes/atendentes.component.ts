9a999e3920f78ec6c359aba493865d4f5160eea9
guilhere_lf <gui_folieti@hotmail.com>
2025-07-10 14:50:17 -0300

    c

--- src/app/atendentes/atendentes.component.ts
+++ src/app/atendentes/atendentes.component.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-atendentes',
   standalone: true,
-  imports: [ReactiveFormsModule, CommonModule, HttpClientModule],
+  imports: [ReactiveFormsModule, RouterModule ,CommonModule, HttpClientModule, FormsModule],
   templateUrl: './atendentes.component.html',
   styleUrl: './atendentes.component.css'
 })
