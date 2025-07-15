7321dbe083deeefe2078a6875011ced91668d71b
guilhere_lf <gui_folieti@hotmail.com>
2025-07-15 16:37:48 -0300

    mudança html e css

--- src/app/download/download.component.ts
+++ src/app/download/download.component.ts
@@ -1,8 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-download',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterModule],
   templateUrl: './download.component.html',
   styleUrl: './download.component.css'
 })
